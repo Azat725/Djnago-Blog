0550efca094f81da3c86ce9577985f46bb3ed424
Azat725 <152897735+Azat725@users.noreply.github.com>
2025-05-15 20:56:19 +0300

    first_commit

---
+++ Blog/mysite/blog/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def post_list(request):
+    posts = Post.published.all()
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': page_obj}
+    )
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    
+    return render(request, 
+                  'blog/post/detail.html',
+                  {'post': post})
+
+
