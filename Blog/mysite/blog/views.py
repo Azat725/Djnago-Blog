66962613ac604b57794a843fb0da7eff2b659c8b
Azat725 <152897735+Azat725@users.noreply.github.com>
2025-05-23 09:18:30 +0300

    add class PostListView

--- Blog/mysite/blog/views.py
+++ Blog/mysite/blog/views.py
@@ -1,33 +1,22 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def post_list(request):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get('page')
-    try:
-        page_obj = paginator.get_page(page_number)
-    except PageNotAnInteger:
-        page_obj = paginator.page(1)
-    
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': page_obj}
-    )
+# Класс-представление для списка постов с пагинацией
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-
+# Функция-представление для детального отображения поста
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-    
-    return render(request, 
-                  'blog/post/detail.html',
-                  {'post': post})
-
-
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    return render(request, 'blog/post/detail.html', {'post': post})
